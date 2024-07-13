f36785826c8e635089930f05baca4d89770514a5
Lucas Aragão Tavares Mota <136651806+Lucasholt124@users.noreply.github.com>
2024-07-13 14:19:18 -0300

    Update index.jsx
    Adicionando função nova no modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -3,7 +3,9 @@ import './style.css';
 export default function Modal({isOpen, setModalOpen, children}) {
   if (isOpen) {
     return (
-    <div className="modal">
+    <div className="modal" onClick={e => {
+        if (e.target.className === "modal1")
+          setModalOpen("Modal was closed">
       <div className='dv-modal'>
         <div className='letra-modalX' onClick={setModalOpen}>X</div>
         <div>{children}</div>
