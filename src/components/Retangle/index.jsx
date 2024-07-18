1effb2b1ff8f7a09318322258671ac5a113e7386
Lucas Aragão Tavares Mota <136651806+Lucasholt124@users.noreply.github.com>
2024-07-18 15:34:04 -0300

    Update index.jsx
    Corrigindo erros de digitação

--- src/components/Retangle/index.jsx
+++ src/components/Retangle/index.jsx
@@ -4,7 +4,7 @@ import './style.css';
 const Retangle = () => {
   return (
     <div className='dv-retangle'>
-      <h4>corde de cabelo</h4>
+      <h4>corte de cabelo</h4>
       <div>
         <h4 id='id-h4-retangle'>R$:20.00</h4>
       </div>
